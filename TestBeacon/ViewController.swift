0323303fa631ed8ae4324ad1077a83a55b45f641
Yoshizumi Ashikawa <ashikawa@Yoshizumi-no-MacBook-Pro.local>
2016-02-01 21:02:44 +0900

    シンプルに

--- TestBeacon/ViewController.swift
+++ TestBeacon/ViewController.swift
@@ -28,14 +28,27 @@ class ViewController: UIViewController,CLLocationManagerDelegate,UITableViewDele
 		setupBeacon()
   }
 
+  //pragma mark - UITableView
+  internal func setupTableView(){
+    print("setupTableView")
+    let topMargin:CGFloat = 20.0
+		tableView = UITableView(frame: CGRectMake(0, topMargin, self.view.frame.width, self.view.frame.height-topMargin))
+    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    tableView.dataSource = self
+    tableView.delegate = self
+    self.view.addSubview(tableView)
+  }
+
   override func viewDidAppear(animated: Bool) {
+
     if((locationManager?.respondsToSelector("requestAlwaysAuthorization")) != nil) {
       if ((locationManager?.respondsToSelector("requestAlwaysAuthorization"))!) == false {
-        locationManager?.requestAlwaysAuthorization()
+				locationManager?.requestAlwaysAuthorization()
       }else {
-        locationManager.startRangingBeaconsInRegion(beaconRegion!)
+				locationManager.startRangingBeaconsInRegion(beaconRegion!)
       }
     }
+
   }
 
   override func viewDidDisappear(animated: Bool) {
@@ -46,22 +59,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate,UITableViewDele
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     
-  }
-
-  //pragma mark - UITableView
-  internal func setupTableView(){
-    print("setupTableView")
-		tableView = UITableView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height))
-    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-    tableView.dataSource = self
-    tableView.delegate = self
-    self.view.addSubview(tableView)
-  }
-
-
-  // cellがクリックされた時の挙動
-  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-
   }
 
   // tableViewのセクション数を指定
@@ -92,46 +89,12 @@ class ViewController: UIViewController,CLLocationManagerDelegate,UITableViewDele
     beaconArray = NSMutableArray()
   }
 
-
   func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
     print("locate check")
     beaconArray.removeAllObjects()
     beaconArray.addObjectsFromArray(beacons)
 		self.tableView.reloadData()
   }
-
-//  // 領域観測が開始した場合
-//  func locationManager(manager: CLLocationManager, didStartMonitoringForRegion region: CLRegion) {
-//    self.sendLocalNotificationForMessage("Start Monitoring Region")
-//  }
-//
-//  // 領域に侵入した場合
-//  func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-//    self.sendLocalNotificationForMessage("Enter Region")
-//
-//    if(region.isMemberOfClass(CLBeaconRegion) && CLLocationManager.isRangingAvailable()) {
-//      self.locationManager?.startRangingBeaconsInRegion(region as! CLBeaconRegion)
-//    }
-//  }
-//
-//  // 領域から退出した場合
-//  func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
-//    self.sendLocalNotificationForMessage("Enter Region")
-//
-//    if(region.isMemberOfClass(CLBeaconRegion) && CLLocationManager.isRangingAvailable()) {
-//      self.locationManager?.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
-//    }
-//  }
-
-  //pragma mark - push
-  func sendLocalNotificationForMessage(message: NSString!) {
-    let localNotification:UILocalNotification = UILocalNotification()
-    localNotification.alertBody = message as String
-    localNotification.fireDate = NSDate()
-    localNotification.soundName = UILocalNotificationDefaultSoundName
-    UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-  }
-
 
 }
 
